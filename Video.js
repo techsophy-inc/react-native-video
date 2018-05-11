a2227be476e78324d01edd5ef9ba6c8217ea6092
Kranthi Kumar <kranthi.julakanti.work>
2018-05-11 14:36:56 +0530

    loaderdelegate as property and drmHeader NSDictionary instead of customData string

--- Video.js
+++ Video.js
@@ -164,7 +164,6 @@ export default class Video extends Component {
     if (uri && uri.match(/^\//)) {
       uri = `file://${uri}`;
     }
-    let customData = this.props.drmHeader ? (this.props.drmHeader.customdata || '' ) : '';
 
     const isNetwork = !!(uri && uri.match(/^https?:/));
     const isAsset = !!(uri && uri.match(/^(assets-library|file|content|ms-appx|ms-appdata):/));
@@ -188,7 +187,7 @@ export default class Video extends Component {
         uri,
         isNetwork,
         isAsset,
-        customData,
+        drmHeader: this.props.drmHeader,
         type: source.type || '',
         mainVer: source.mainVer || 0,
         patchVer: source.patchVer || 0,
